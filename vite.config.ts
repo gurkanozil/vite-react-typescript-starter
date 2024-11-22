90a932fbdeb57d4322ff6954cadc9ea84c8b1e56
gurkanozil <gurkanozil@live.nl>
2024-11-22 20:04:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gurkanozil.github.io/', // Base URL for your repository
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: './', // This ensures assets are loaded correctly on GitHub Pages
 });
